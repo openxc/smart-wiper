d5aa4bb492103b6cad5d32bffc919d3ee12d6e43
Yonathan Redda <yredda@ford.com>
2014-08-14 13:29:43 -0700

    clean up of multiple view files

--- Android/ConnectedWiper/src/com/ford/svl/connectedwiper/MainActivity.java
+++ Android/ConnectedWiper/src/com/ford/svl/connectedwiper/MainActivity.java
@@ -87,7 +87,7 @@ public class MainActivity extends Activity implements OnClickListener, OnChecked
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main);
         
         status 	  = (TextView) findViewById(R.id.status);
         heavyRain = (TextView) findViewById(R.id.heavyRain);
